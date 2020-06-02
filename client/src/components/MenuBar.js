92e169837c5aa639487189ae7b16c5e55f29861c
Alan Budiarto <alanwbudiarto@gmail.com>
2020-06-02 16:04:46 +0700

    MenuBar Login

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useQuery } from '@apollo/react-hooks';
 import { Link } from 'react-router-dom';
 import '../styles/MenuBar.scss'
+import { GET_USER_DATA } from "../schema";
+import Avatar from '@material-ui/core/Avatar';
 
 export default ({ setShowLoginRegister }) => {
+
+    const { data } = useQuery(GET_USER_DATA);
+    useEffect(() => {
+        if (data && data.userData);
+    }, [data])
+
     return (
         <div className="menu-bar">
             <div className="left bar">
@@ -24,12 +33,22 @@ export default ({ setShowLoginRegister }) => {
                 </Link>
             </div>
             <div className="right bar">
-                <div className="btn menubtn" onClick={() => { setShowLoginRegister('login') }}>
-                    <p className="menutext">Login</p>
-                </div>
-                <div className="btn menubtn" onClick={() => { setShowLoginRegister('register') }}>
-                    <p className="menutext">Register</p>
-                </div>
+                {(!data || !data.userData) &&
+                    <>
+                        <div className="btn menubtn" onClick={() => { setShowLoginRegister('login') }}>
+                            <p className="menutext">Login</p>
+                        </div>
+                        <div className="btn menubtn" onClick={() => { setShowLoginRegister('register') }}>
+                            <p className="menutext">Register</p>
+                        </div>
+                    </>
+                }
+
+                {data && data.userData &&
+                    <>
+                        <Avatar className="avatar" title={data.userData.username} >{data.userData.username[0].toUpperCase()}</Avatar>
+                    </>
+                }
             </div>
         </div>
     )
