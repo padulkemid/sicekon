c11a520a09076fbd2f6bdaaca375d21f72351067
Alan Budiarto <alanwbudiarto@gmail.com>
2020-06-02 16:02:51 +0700

    Edited Inputs

--- client/src/components/GenderInput.js
+++ client/src/components/GenderInput.js
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 
-export default function ({ onChange }) {
-
-    const [gender, setGender] = useState('');
-
-    const handleChange = (event) => {
-        setGender(event.target.value);
-        onChange(event);
-    };
+export default function ({ onChange, value }) {
 
     return (
-        <FormControl variant="outlined" className="gender-input input-field">
+        <FormControl variant="outlined" className="sex-input input-field">
             <InputLabel id="select-outlined-label">Gender</InputLabel>
             <Select
                 labelId="select-outlined-label"
                 id="select-outlined"
-                value={gender}
-                onChange={handleChange}
+                value={value}
+                onChange={onChange}
                 label="Gender"
             >
+                <MenuItem value={''}></MenuItem>
                 <MenuItem value={'male'}>Male</MenuItem>
                 <MenuItem value={'female'}>Female</MenuItem>
             </Select>
--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
 import TextField from '@material-ui/core/TextField';
 
-export default function ({ onChange, label, isAge }) {
-
-    const [text, setText] = useState('');
-
-    const handleChange = (event) => {
-        setText(event.target.value);
-        onChange(event);
-    };
-
+export default function ({ onChange, label, isAge, value }) {
     return (
         <TextField className="input-field" id="outlined-basic"
-            type={isAge ? 'number' : 'text'} label={label} value={text} onChange={handleChange} variant="outlined" />
+            type={isAge ? 'number' : 'text'} label={label} value={value} onChange={onChange} variant="outlined" />
     );
 }
